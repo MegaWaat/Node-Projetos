f86e7766cbed28be853c828a26c56848e7e418e8
Kevin C.Camargo <kevincavalheiro@hotmail.com>
2020-09-18 16:58:32 -0300

    teste

--- siegaec/src/App.vue
+++ siegaec/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Bem vindo.js App"/>
+    <HelloWorld msg="Bem vindo a.js App"/>
   </div>
 </template>
 
